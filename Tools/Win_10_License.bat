1091337535d081bf372392ca8f2de25456fa63b7
Yextep <114537444+Yextep@users.noreply.github.com>
2023-09-06 10:16:50 -0600

    Update Win_10_License.bat

--- Tools/Win_10_License.bat
+++ Tools/Win_10_License.bat
@@ -1,1 +1,12 @@
+@REM Se necesita ejecutar como administrador para que funcione :p
+color 06
 
+slmgr.vbs /upk
+slmgr.vbs /cpky
+slmgr.vbs /ckms
+DISM /online /Get-TargetEditions
+sc config LicenseManager start= auto & net start LicenseManager
+sc config wuauserv start= auto & net start wuauserv
+changepk.exe /productkey VK7JG-NPHTM-C97JM-9MPGT-3V66T
+
+exit
